297a9784bb0c2ceee878d8586341a5cf8bfd109a
Edinaldo Rodrigues <edinaldorsaws@gmail.com>
2026-03-16 15:05:21 +0000

    update text button

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        New Task
+        Add nova Tarefa
       </button>
       
       <Modal
